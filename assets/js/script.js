cd6434f19dd44895ed2865af24dd1a833f8c778f
marinaP <mar.pavlovic@gmail.com>
2021-11-02 11:05:32 +0100

    Update gsap to newer v and update social icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,14 @@
-let navigation = new gsap.timeline({paused:true, reversed:true});
-navigation.to("#navigationWrap", 0.5, {opacity: 1, display: 'block'})
-          .to(".navbar", 0.3, {opacity: 0}, "-=0.1")
-          .to(".close", 0.3, {display: "block", opacity: 1}, "-=0.1")
-          .from(".menu", 0.5, {opacity: 0, y: 30})
-          .from(".social", 0.5, {opacity: 0});
+let navigation = gsap.timeline({
+  paused: true, 
+  reversed: true
+});
 
-$(".navbar, .close").click (function() {
+navigation.to('#navigationWrap', 0.5, {opacity: 1, display: 'block'})
+          .to('.navbar', 0.3, {opacity: 0}, '-=0.1')
+          .to('.close', 0.3, {display: 'block', opacity: 1}, "-=0.1")
+          .from('.menu', 0.5, {opacity: 0, y: 30})
+          .from('.social', 0.5, {opacity: 0});
+
+$('.navbar, .close').click (function() {
   navigation.reversed() ? navigation.play() : navigation.reverse();
 })
