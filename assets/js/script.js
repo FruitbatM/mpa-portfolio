f9262f23a7beba5d0e45adb4f525503df563964d
marinaP <mar.pavlovic@gmail.com>
2021-11-05 12:29:03 +0100

    Update overlay navigation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,93 @@
-let navigation = gsap.timeline({
-  paused: true, 
-  reversed: true
+$(document).ready(function() {
+	$(".open-menu").on("click", function() {
+		$(".overlay").toggleClass("open");
+		$(".contain").toggleClass("change");
+	});
 });
 
-navigation.to('#navigationWrap', 0.5, {opacity: 1, display: 'block'})
-          .to('.navbar', 0.3, {opacity: 0}, '-=0.1')
-          .to('.close', 0.3, {display: 'block', opacity: 1}, "-=0.1")
-          .from('.menu', 0.5, {opacity: 0, y: 30})
-          .from('.social', 0.5, {opacity: 0});
+$(document).ready(function() {
+	// opens and closes overlay nav from animated buttons
 
-$('.navbar, .close').click (function() {
-  navigation.reversed() ? navigation.play() : navigation.reverse();
-})
+	$(".menu-icon").on("click", function() {
+		$(".overlay").toggleClass("open");
+		$(".contain").toggleClass("change");
+	});
+
+	// closes overlay from overlay links
+
+	$(".overlay .overlay-menu li").on("click", function() {
+		$(".overlay").toggleClass("open");
+		$(".contain").toggleClass("change");
+	});
+
+	// Scroll to top button appears
+
+	$(window).scroll(function() {
+		if ($(this).scrollTop() > 900) {
+			$(".btt img").fadeIn(500);
+		} else {
+			$(".btt img").fadeOut(500);
+		}
+	});
+
+	// Scroll to top scrolls up
+
+	$(".btt img").click(function() {
+		$("html, body").animate({ scrollTop: 0 }, 800);
+		return false;
+	});
+
+	$("footer")
+		.find("a")
+		.click(function(e) {
+			e.preventDefault();
+			var section = $(this).attr("href");
+			$("html, body").animate({ scrollTop: $(section).offset().top });
+		});
+});
+
+// Select all links with hashes
+
+$('a[href*="#"]')
+	// Remove links that don't actually link to anything
+	.not('[href="#"]')
+	.not('[href="#0"]')
+	.click(function(event) {
+		// On-page links
+		if (
+			location.pathname.replace(/^\//, "") == this.pathname.replace(/^\//, "") &&
+			location.hostname == this.hostname
+		) {
+			// Figure out element to scroll to
+			var target = $(this.hash);
+			target = target.length ? target : $("[name=" + this.hash.slice(1) + "]");
+			// Does a scroll target exist?
+			if (target.length) {
+				// Only prevent default if animation is actually gonna happen
+				event.preventDefault();
+				$("html, body").animate(
+					{
+						scrollTop: target.offset().top
+					},
+					1000,
+					function() {
+						// Callback after animation
+						// Must change focus!
+						var $target = $(target);
+						$target.focus();
+						if ($target.is(":focus")) {
+							// Checking if the target was focused
+							return false;
+						} else {
+							$target.attr("tabindex", "-1"); // Adding tabindex for elements not focusable
+							$target.focus(); // Set focus again
+						}
+					}
+				);
+			}
+		}
+	});
+
 
 // Back to Top Arrow - code with modification taken from: https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
 mybutton = document.getElementById('arrow_2top');
