58d55e4eb4a531c3fb67becedcdc97085bb7c79e
marinaP <mar.pavlovic@gmail.com>
2022-02-11 14:26:08 +0100

    Simplify overlay navigation function

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,36 +14,10 @@ $(document).ready(function() {
 	});
 
 	// closes overlay from overlay links
-
 	$(".overlay .overlay-menu li").on("click", function() {
 		$(".overlay").toggleClass("open");
 		$(".contain").toggleClass("change");
 	});
-
-	// Scroll to top button appears
-
-	$(window).scroll(function() {
-		if ($(this).scrollTop() > 900) {
-			$(".btt img").fadeIn(500);
-		} else {
-			$(".btt img").fadeOut(500);
-		}
-	});
-
-	// Scroll to top scrolls up
-
-	$(".btt img").click(function() {
-		$("html, body").animate({ scrollTop: 0 }, 800);
-		return false;
-	});
-
-	$("footer")
-		.find("a")
-		.click(function(e) {
-			e.preventDefault();
-			var section = $(this).attr("href");
-			$("html, body").animate({ scrollTop: $(section).offset().top });
-		});
 });
 
 // Select all links with hashes
